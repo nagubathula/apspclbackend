264b02412d8b59608d4696599083e4b8f7cb368f
nagubathula <satyasai2108@gmail.com>
2024-07-04 19:00:03 +0530

    Changes

--- models/User.js
+++ models/User.js
@@ -1,37 +1,20 @@
+// models/User.js
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-    creds: {
-        type: String,
-        required: true
-    },
-    name: {
-        type: String,
-        required: true
-    },
-    dateOfBirth: {
-        type: Date,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    role: {
-        type: String,
-        enum: ['Admin', 'User'],
-        default: 'Admin' // Set the default value to 'Admin'
-    },
-    designation: {
-        type: String,
-        required: true
-    }
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
+module.exports = mongoose.model('User', UserSchema);
