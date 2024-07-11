d28f48041bb0a7f778dd7b787620a5e6465360f2
nagubathula <satyasai2108@gmail.com>
2024-07-11 15:40:48 +0530

    Revert "backend env"
    This reverts commit c6ae59fcca5821f6a0a53faa76be0b9380f2dbf2.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// backend/app.js
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
@@ -5,8 +6,6 @@ const connectDB = require("./db");
 const peopleRoutes = require("./routes/peopleRoutes");
 const reportRoutes = require("./routes/reportRoutes");
 const authRoutes = require('./routes/authRoutes');
-require('dotenv').config();
-
 
 const app = express();
 
@@ -15,16 +14,16 @@ connectDB();
 
 // Middleware
 const corsOptions = {
-  origin: process.env.CORS_ORIGIN, // Allow requests from the Next.js frontend
+  origin: 'http://localhost:3000/', // Allow requests from the Next.js frontend
   optionsSuccessStatus: 200,
 };
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use("/reports", reportRoutes);
-app.use("/people", peopleRoutes);
-app.use('/auth', authRoutes);
+app.use("/api/reports", reportRoutes);
+app.use("/api/people", peopleRoutes);
+app.use('/api/auth', authRoutes);
 
 // Serve static files from public/uploads
 app.use(
@@ -36,7 +35,6 @@ app.use(
   '/uploads/peopleData',
   express.static(path.join(__dirname, '..', 'apspcl', 'public', 'peopleData'))
 );
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
