d08ae6685268477f2f51a0ff5b3104e86b6f2823
Satya Sai <satyasai2108@gmail.com>
2025-01-28 15:53:15 +0530

    changes

--- models/boardofdirectors.js
+++ models/boardofdirectors.js
@@ -11,11 +11,11 @@ const BoardOfDirectorsSchema = new mongoose.Schema({
   },
   image: {
     type: String, // Path to the uploaded image
-    required: true,
+  
   },
   filepath: {
     type: String, // Path where the image file is stored
-    required: true, // Set to true if the filepath is mandatory
+  // Set to true if the filepath is mandatory
   },
 });
 
